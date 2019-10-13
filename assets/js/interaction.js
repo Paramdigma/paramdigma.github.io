fe695aecab2b443d4e3c4af432923c42d4a35bd2
Alan Rynne <alan@rynne.es>
2019-10-13 16:49:39 +0200

    Initial commit with jekyll template

--- assets/js/interaction.js
+++
@@ -1,9 +0,0 @@
-// Toggle visibility of modal contact form on main page
-function toggleContactForm(){
-    document.getElementById("contact-form").classList.toggle("is-active");
-}
-
-// Toggle visibility of modal about us on main page
-function toggleAboutUsModal(){
-  document.getElementById("about-us").classList.toggle("is-active");
-}
---
+++ assets/js/particles-init.js
@@ -0,0 +1,7 @@
+---
+---
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '{{site.url}}/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
